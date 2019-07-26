47a4b40566aabaf837d37499ba3e209378d6a2fe
Prog <teknisk@game.unf.dk>
2019-07-26 22:22:30 +0200

    End of day 1 progammering

---
+++ GDC2019/Assets/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject Hook;
+    public float HookSpeed = 100f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject instHook = Instantiate(Hook, transform.position, Quaternion.identity);
+            Rigidbody instHookRigidbody = instHook.GetComponent<Rigidbody>();
+            instHookRigidbody.AddForce(Vector3.right * HookSpeed);
+
+        }
+    }
+
+
+}
