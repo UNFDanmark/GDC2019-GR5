dd66df5646bf90fc1e72e16cf879b703427910b8
Prog <teknisk@game.unf.dk>
2019-07-27 23:17:12 +0200

    end of day 2 commit

--- GDC2019/Assets/HookScript.cs
+++ GDC2019/Assets/HookScript.cs
@@ -4,69 +4,176 @@ using UnityEngine;
 
 public class HookScript : MonoBehaviour
 {
-    public Rigidbody rb;
+    bool Hooked = false;
+    bool Fired = false;
+    int DragAble = 0;
+
     public float HookSpeed = 100f;
     public float PullForce = 10f;
-    Rigidbody ColRig;
+    public float DragForce;
+    public float AimSpeed;
     public GameObject Player;
+    public GameObject Target;
+    //Vector3 PlayerPos = new Vector3(Player.transform.position.x, Player.transform.position.y, 0);
+    Vector3 PlayerPos = new Vector3(0, 0, 0);
+    
+
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.isKinematic = true;
+        Physics.IgnoreLayerCollision(8, 9);
+        Physics.IgnoreLayerCollision(8, 10);
     }
 
 
     void Update()
     {
+        PushOutOfBox();
+        Aim();
         HookShot();
-        
-    }
+        Release();
 
- 
-    void OnCollisionEnter(Collision col)
-    {
-       
+        Vector3 AimDir = transform.position - Player.transform.position;
+        Vector3 AimNormDir = AimDir.normalized;
 
-        if (col.gameObject.CompareTag("Drag"))
+        //sender hooken afsted
+        if (Fired && !Hooked)
         {
-            col.gameObject.transform.parent = gameObject.transform;
-            ColRig = col.gameObject.GetComponent<Rigidbody>();
-            ColRig.isKinematic = true;
-
-            Vector3 DragWay;
-            float xpos = gameObject.transform.position.x;
-            float ypos = gameObject.transform.position.y;
-            float PlayerXPos = Player.transform.position.x;
-            float PlayerYPos = Player.transform.position.y;
-
-            DragWay = new Vector3((PlayerXPos - xpos)/Mathf.Sqrt((PlayerXPos - xpos) * (PlayerXPos - xpos) + (PlayerYPos - ypos) * (PlayerYPos - ypos)), 
-                                  (PlayerYPos - ypos)/Mathf.Sqrt((PlayerXPos - xpos) * (PlayerXPos - xpos) + (PlayerYPos - ypos) * (PlayerYPos - ypos)));
-            rb.AddForce(DragWay * PullForce);
-
-
-
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            gameObject.GetComponent<Rigidbody>().AddForce(AimDir * HookSpeed);
         }
 
-        if (col.gameObject.CompareTag("UnDrag"))
+        //trækker hooken tilbage
+        if (Fired && Hooked && DragAble == 1)
         {
+            float step = DragForce * Time.deltaTime;
+
+            Vector3 dir = Player.transform.position - transform.position;
+            Vector3 NormDir = dir.normalized;
+            GetComponent<Rigidbody>().AddForce(NormDir * step);
+
+            print(NormDir);
+ 
+        }
+        
+        //Trækker player til hook
+        if (Fired && Hooked && DragAble == 2)
+        {
+            float step = PullForce * Time.deltaTime;
+
+            Vector3 dir = transform.position - Player.transform.position;
+            Vector3 NormDir = dir.normalized;
+            Player.GetComponent<Rigidbody>().AddForce(NormDir * step);
+          
+            print(-NormDir);
+        }
+    }
+
+    
+    void OnCollisionEnter(Collision col)
+    {   
+        if(!Hooked && Fired)
+        {
+            Target = col.gameObject;
+
+            //sætter hooken på kassen, og annulere fart/moment
+            if (Target.CompareTag("Drag") && Fired)
+            {
+                Hooked = true;
+                DragAble = 1;
+
+                Target.AddComponent<CharacterJoint>();
+                Target.GetComponent<CharacterJoint>().connectedBody = gameObject.GetComponent<Rigidbody>();
+                GetComponent<Rigidbody>().velocity = Vector3.zero;
+                Target.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            }
+
+            if (Target.CompareTag("UnDrag") && Fired)
+            {
+                Hooked = true;
+                DragAble = 2;
+                Target.AddComponent<FixedJoint>();
+                Target.GetComponent<FixedJoint>().connectedBody = gameObject.GetComponent<Rigidbody>();
+                GetComponent<Rigidbody>().velocity = Vector3.zero;
+            }
+        }
+       
+    }
+
+    private void OnTriggerEnter(Collider trig)
+    {
+        //slipper kassen og resetter hooken
+        if(Hooked && Fired)
+        {
+            Hooked = false;
+            Fired = false;
+            DragAble = 0;
+            Destroy(Target.GetComponent<CharacterJoint>());
+            Destroy(Target.GetComponent<FixedJoint>());
+            print(Target.name);
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
             
         }
 
-        /*
-        if (col.gameObject.CompareTag("Player"))
-        {
-            ColRig.isKinematic = false; 
-        }
-        */
     }
 
     //Skyde Hook
     void HookShot()
-    {
+    {   
+        //input for at skyde hooken afsted
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.isKinematic = false;
-            rb.AddForce(Vector3.right * HookSpeed);
+            Fired = true;
+        }
+    }
+
+    //drej hook om player
+    void Aim()
+    {
+        if(!Hooked && !Fired)
+        {
+            /* if (transform.position.y >= Player.transform.position.y - 0.01)
+             {
+                 transform.RotateAround(Player.transform.position, Vector3.back, AimSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+             }
+
+             else
+             {
+                 transform.RotateAround(Player.transform.position, Vector3.back, AimSpeed * -Input.GetAxis("Horizontal") * Time.deltaTime);
+             }*/
+
+            transform.RotateAround(Player.transform.position, Vector3.back, AimSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+        }
+        
+
+    }
+
+    float ChekPos()
+    {
+        Vector3 PlayerHookDis = transform.position - Player.transform.position;
+        float temp = PlayerHookDis.magnitude;
+        return temp;
+    }
+
+    void PushOutOfBox()
+    {
+        if (ChekPos() < 0.5f)
+        {
+            /*Vector3 dir = transform.position - Player.transform.position;
+            Vector3 NormDir = dir.normalized;
+            transform.position = NormDir * 2f;*/
+            transform.localPosition = new Vector3(0f, 0f, 0.5f);
+
+        }
+    }
+
+  
+
+    void Release()
+    {
+        if(Hooked && Fired && Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(Target.GetComponent<CharacterJoint>());
+            Destroy(Target.GetComponent<FixedJoint>());
         }
     }
 }
--- GDC2019/Assets/PlayerScript.cs
+++ GDC2019/Assets/PlayerScript.cs
@@ -4,25 +4,6 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public GameObject Hook;
-    public float HookSpeed = 100f;
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GameObject instHook = Instantiate(Hook, transform.position, Quaternion.identity);
-            Rigidbody instHookRigidbody = instHook.GetComponent<Rigidbody>();
-            instHookRigidbody.AddForce(Vector3.right * HookSpeed);
-
-        }
-    }
 
 
 }
