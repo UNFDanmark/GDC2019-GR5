150f3cbef07f0a4698c4ed18eaab47cb655247e6
Prog <teknisk@game.unf.dk>
2019-07-29 18:19:06 +0200

    Går til næste lvl

--- GDC2019/Assets/Scripts/PlayerScript.cs
+++ GDC2019/Assets/Scripts/PlayerScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class PlayerScript : MonoBehaviour
     {
         if (other.CompareTag("Finish"))
         {
-            Application.LoadLevel(Application.loadedLevel);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
