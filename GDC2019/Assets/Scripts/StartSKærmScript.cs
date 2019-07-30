81503a674e4fb823ddc2320333c3294bcfe857d2
Prog <teknisk@game.unf.dk>
2019-07-30 22:54:37 +0200

    Control tavle

--- GDC2019/Assets/Scripts/StartSKærmScript.cs
+++ GDC2019/Assets/Scripts/StartSKærmScript.cs
@@ -2,22 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartSKærmScript : MonoBehaviour
 {
+
+    public GameObject Tavle;
+    public GameObject BLur;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Tavle.SetActive(false);
+        BLur.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene(1);
             Time.timeScale = 1;
         }
+    }
+
+    public void Cont()
+    {
+        Tavle.SetActive(true);
+        BLur.SetActive(true);
     }
 }
